38d240b1945498281b074a3a76797e4564a2c475
rainfly <rainflee@126.com>
2017-02-03 12:30:54 +0800

    update comment for t2.cpp

--- t2.cpp
+++ t2.cpp
@@ -3,34 +3,34 @@
 //  OpenGLDemo
 //
 //  Created by 919575700@qq.com on 16/4/24.
-//  Copyright © 2016? Jiangxh. All rights reserved.
+//  Copyright © 2016年 Jiangxh. All rights reserved.
 //
-#include <GL/glut.h> // GLUT???
+#include <GLUT/GLUT.h> // GLUT窗口库
 
-// ?????????
+// 自定义一个绘制函数
 void display()
 {
-    // ??
+    // 清空
     glClear(GL_COLOR_BUFFER_BIT);
-    // ?????????
+    // 开始绘制一个多边形
     glBegin(GL_POLYGON);
-    // ???????????
-    glVertex2f(-1, -1);
-    glVertex2f(1, -1);
-    glVertex2f(0, -0);
-    //????
+    // 多边形的顶点（三角形）
+    glVertex2f(-0.5, -0.5);
+    glVertex2f(0, 0.5);
+    glVertex2f(0.5, -0.5);
+    //绘制结束
     glEnd();
     glFlush();
 }
 
-// ????
+// 程序入口
 int main(int argc, char ** argv)
 {
-    //?????
+    //窗口初始化
     glutInit(&argc, argv);
-    //??????Demo???
+    //创建一个名为Demo的窗口
     glutCreateWindow("Demo");
-    //????
+    //绘图函数
     glutDisplayFunc(display);
     glutMainLoop();
 }
