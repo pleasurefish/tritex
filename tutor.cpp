9ec3525d749304d05f301c430e008bf8edcfd655
rainfly <rainflee@126.com>
2017-02-03 16:14:24 +0800

    add tutorial cpp

---
+++ tutor.cpp
@@ -0,0 +1,20 @@
+
+#include <GL/glut.h>
+
+void RenderScenceCB(void)
+{
+	glClear(GL_COLOR_BUFFER_BIT);
+	glutSwapBuffers();
+}
+
+int main(int argc, char *argv)
+{
+	glutInit(&argc, argv);
+	glutInitDisplayMode(GLUT_DOUBLE | GLUT_RGBA);
+	glutInitWindowSize(1024, 768);
+	glutInitWindowPosition(100, 100);
+	glutCreateWindow("Tutorial 01");
+	glutDisplayFunc(RenderSceneCB);
+	glClearColor(0.0f, 0.0f, 0.0f, 0.0f);
+	glutMainLoop();
+}
