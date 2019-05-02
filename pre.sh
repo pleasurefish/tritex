dab9a8f9b58d7ab0ea86e3f83be4d5bb87f1c1e4
pls <pleasurefish@126.com>
2019-05-02 23:49:03 +0800

    update pre script

--- pre.sh
+++ pre.sh
@@ -1,3 +1,6 @@
 #!/bin/sh
+sudo apt-get update
 sudo apt-get install --yes aptitude 
 sudo apt-get install --yes freeglut3 freeglut3-dbg freeglut3-dev
+git config --global user.email "pleasurefish@126.com"
+git config --global user.name "pls"
